f4754c9b0e056d933fc4fbc12d67d55483cc26b1
Lev-Alan <summer.helen.15@gmail.com>
2025-04-27 00:31:10 +1000

    Homework2
    Вторая часть домашнего задания по теме строки и функции

--- 1-converter/main.go
+++ 1-converter/main.go
@@ -2,12 +2,25 @@ package main
 
 import "fmt"
 
+const USDtoRUB = 81.14
+const USDtoEUR = 0.88          
 func main() {
-	const USDtoEUR = 0.88                   // константа конвертации из 1 USD to EUR
-	const USDtoRUB = 81.14                  // константа конвертации из 1 USD to RUB
-	fmt.Println("Конвертер валют")
-	n := 3.0                                // n количество евро, которое нам необходимо конвертировать в рубли
-	EUR := n * USDtoRUB / USDtoEUR          // формула для конвертации Евро в рубли
-	fmt.Printf("Ваш результат в рублях: %.02f", EUR)
-
+	fmt.Println(" Конвертер валют ")
+	n := getUSerInput() 
+	EUR := EURconverter(n, USDtoRUB, USDtoEUR)   // количество евро, которое нужно конвертировать в рубли
+	outputResult(EUR)
+}
+func getUSerInput ()(n float64){
+	fmt.Println("Введите количество евро, которое необходимо конвертировать в рубли")
+	fmt.Scan(&n)
+	return n
+}
+func EURconverter (n, USDtoRUB, USDtoEUR float64)(float64){
+	EUR := n * USDtoRUB / USDtoEUR // формула для конвертирования евро рубли
+	return EUR
+	
+}
+func outputResult(EUR float64){
+	result := fmt.Sprintf("Ваш результат в рублях: %.02f", EUR)
+	fmt.Print(result)
 }
